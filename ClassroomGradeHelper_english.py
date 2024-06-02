fec4c626ea07513e6cbd14ad3e31e80c79193484
Vitor Renan <167242612+VitorRenanSD@users.noreply.github.com>
2024-06-02 08:54:37 -0300

    Update ClassroomGradeHelper_english.py
    Improvement in the visualization of the code by the user, more organized the final Print with a fixed value of characters of the articles in 25, thus the Media and Status of all exclusive on the same line. Bold yellow color placed on fixed words as well

--- ClassroomGradeHelper_english.py
+++ ClassroomGradeHelper_english.py
@@ -4,7 +4,7 @@ from time import sleep
 
 num_subjects = int(input('Number of subjects: '))
 school_average = float(input('School average: '))
-print('\033[33m------Basic information saved!------\033[0m')
+print('\033[33m-------Basic information saved!------\033[0m')
 sleep(0.4)
 
 subjects = []
@@ -12,12 +12,12 @@ subjects = []
 for i in range(num_subjects):
     subject = str(input(f'Name of the {i+1}th subject: '))
     grades = str(input(f'Grades of {subject} exams (separated by space): ')).split()
-    print('\033[33m------Subject/Grades saved!------\033[0m')
+    print('\033[33m---------Subject/Grades saved!--------\033[0m')
     sleep(0.4)
     grades = [float(grade) for grade in grades]
     subjects.append([subject] + grades)
 
-print('\033[33m----------Processing...----------\033[0m')
+print('\033[33m-------------Processing...------------\033[0m')
 sleep(0.7)
 
 for subject in subjects:
@@ -25,7 +25,7 @@ for subject in subjects:
     grades = np.array(subject[1:])
     average = np.mean(grades)
     status = '\033[32mApproved\033[0m' if average >= school_average else '\033[31mRecovery\033[0m'
-    print(f'Subject: {name}, Average: {average:.2f}, Status: {status}')
+    print(f'\033[1;33mSubject:\033[0m {name:25} \033[1;33mAverage:\033[0m {average:.2f} \033[1;33mStatus:\033[0m {status}')
 
 sleep(2.0)
 print('\033[35mCreated by: VitorRenanSD\033[0m')
